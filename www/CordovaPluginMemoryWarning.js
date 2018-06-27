30be20150e61c43a8c42aaf43281b288c17abbc7
Uday Kiran Ailapaka <AUK4GIT@users.noreply.github.com>
2018-06-27 15:30:33 +0530

    added getUsableSpace method

--- www/CordovaPluginMemoryWarning.js
+++ www/CordovaPluginMemoryWarning.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.isMemoryUsageUnsafe = function(success, fail) {
     exec(success, fail, "CordovaPluginMemoryWarning", "isMemoryUsageUnsafe");
 };
+
+exports.getUsableSpace = function(success, fail) {
+    exec(success, fail, "CordovaPluginMemoryWarning", "getUsableSpace");
+};
