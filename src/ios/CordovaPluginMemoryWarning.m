841bc93faf0895e79e196dafd21adb12796554e6
Uday Kiran Ailapaka <AUK4GIT@users.noreply.github.com>
2018-06-27 15:10:52 +0530

    getUsableSpace code added

--- src/ios/CordovaPluginMemoryWarning.m
+++ src/ios/CordovaPluginMemoryWarning.m
@@ -23,4 +23,21 @@
     NSLog(@"cordova-plugin-memory-warning: received a memory warning");
 }
 
+- (void)getUsableSpace:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    //NSString* echo = [command.arguments objectAtIndex:0];
+    
+    @try{
+        long long freeSpace = [[[[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:nil] objectForKey:NSFileSystemFreeSize] longLongValue];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: [NSString stringWithFormat:@"%lld", freeSpace]];
+    }
+    @catch (NSException *exception){
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
 @end
